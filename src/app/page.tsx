5c56517ecff91f8ffd764af6453e4eb1b0d0e780
Nick Sullivan <ncksllvn@gmail.com>
2024-02-04 19:51:54 -0500

    Source content using gray-matter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,49 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import fs from "fs";
+import Head from "next/head"
+import path from 'node:path';
+import matter from "gray-matter";
+import yaml from "js-yaml";
 
-export default function Home() {
+const homeContentPath = path.join(process.cwd(), "content/home.md");
+
+export type HomeContent = {
+  readonly title: string;
+  readonly date: string;
+  readonly cats: { description: string; name: string }[];
+};
+
+async function getData() {
+  const fileContents = fs.readFileSync(homeContentPath, "utf8");
+
+  const matterResult = matter(fileContents, {
+    engines: {
+      yaml: (s) => yaml.load(s, { schema: yaml.JSON_SCHEMA }) as object,
+    },
+  });
+
+  const home = matterResult.data as HomeContent;
+
+  return home;
+};
+
+export default async function Home() {
+  const home = await getData();
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    <>
+      <Head>
+        <script async src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+      </Head>
+      <article>
+        <h1>{home.title}</h1>
+        <ul>
+          {home.cats.map((cat, k) => (
+            <li key={k}>
+              <h2>{cat.name}</h2>
+              <p>{cat.description}</p>
+            </li>
+          ))}
+        </ul>
+      </article>
+    </>
+  )
 }
